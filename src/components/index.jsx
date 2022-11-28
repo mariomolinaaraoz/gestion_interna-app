ab4c2245a42ab964a4e27ceff390cf5ab3ad7e28
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2022-11-27 21:06:48 -0300

    fix SideBar name

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,4 @@
 export { default as Navbar } from './Navbar/Navbar';
-export { default as Sidebar } from './Dashboard/Sidebar';
+export { default as SideBar } from './Dashboard/SideBar';
 export { default as NewOrder } from './Cut&Folding/NewOrder';
 export { default as ColorProyect } from './Cut&Folding/ColorProyect';
