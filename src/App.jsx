a8339b9be3b9b3a5f1e914453f03c7a8471901ab
mariomolinaaraoz <mariomolinaaraoz@gmail.com>
2023-04-25 10:23:10 -0300

    avance 4

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
   }, []);
 
   return (
-    <div className="container-fluid">      
+    <div className="">      
       <Routes>
         <Route
           path="/"
